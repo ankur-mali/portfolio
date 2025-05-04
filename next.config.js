432e31172ec34d28d1f2e24d16d13b96700cff7c
70966_Ankur <ankurmali02@gmail.com>
2025-05-04 18:17:35 +0200

    ssl changes

--- next.config.js
+++ next.config.js
@@ -11,6 +11,16 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        has: [{ type: 'host', value: 'ankurmali.com' }],
+        permanent: true,
+        destination: 'https://ankurmali.com/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
